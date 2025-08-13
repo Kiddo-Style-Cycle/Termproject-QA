b41f465649ab48dc45596a5a5c5c183a30dfaafd
qtrngn <nguyentrucquynh98@gmail.com>
2025-08-13 13:40:23 -0700

    hero challenge finished

--- src/pages/Act.jsx
+++ src/pages/Act.jsx
@@ -1,11 +1,13 @@
 import StyleLeader from "../components/ActSections/StyleLeader"
 import FashionChallenge from "../components/ActSections/FashionChallenge"
+import SuperPower from "../components/ActSections/SuperPower"
 
 const Act = () => {
   return (
     <>
     <StyleLeader/>
     <FashionChallenge/>
+    <SuperPower/>
     </>
   )
 }
