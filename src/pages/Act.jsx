ee4fec0ee846a7cf2a24125960be8c4e994f4507
Negin-code <iamneginasem@gmail.com>
2025-08-13 13:39:11 -0700

    Refactor Act page to include multiple new components (HeroA, FashionHero, HomeFashion, TeamUp, StyleLeader, FashionChallenge) for improved layout and content variety.

--- src/pages/Act.jsx
+++ src/pages/Act.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import HeroA from "../components/ActSections/HeroA";
+import FashionHero from "../components/ActSections/FashionHero";
+import HomeFashion from "../components/ActSections/HomeFashion";
+import TeamUp from "../components/ActSections/TeamUp";  
+import StyleLeader from "../components/ActSections/StyleLeader"
+import FashionChallenge from "../components/ActSections/FashionChallenge"
 
 const Act = () => {
   return (
-    <div>Act</div>
-  )
-}
+    <div className="min-h-screen w-screen">
+      <HeroA />
+      <FashionHero />
+      <HomeFashion />
+      <TeamUp />
+      <StyleLeader/>
+      <FashionChallenge/>
+
+    </div>
+  );
+};
 
 export default Act
